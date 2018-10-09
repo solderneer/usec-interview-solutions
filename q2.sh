d8e8553677db5c06a8b7d640f699ca6e1edf551a
Sudharshan <sudhar393@gmail.com>
2018-10-09 11:44:02 +0800

    fixed some formatting

--- q2.sh
+++ q2.sh
@@ -5,8 +5,10 @@ if [ $# -ne 2 ]; then
   exit 1
 fi
 
+# TODO: Input validation to check for IP address
 while IFS='' read -r line || [[ -n "$line" ]]; do
-    echo "Text read from file: $line"
+    echo "IP address to be blocked: $line"
     iptables -A INPUT -s $line -j DROP
     echo "iptables -D INPUT -s $line -j DROP" | at now + $2 minutes
+    echo "IP address("$lines") has been blocked for "$2" minutes\n"
 done < "$1"
