119f6e3f26cbc5f6c025f208b6df76eae4fb51c4
Sudharshan <sudhar393@gmail.com>
2018-10-09 11:42:14 +0800

    finished q2

--- q2.sh
+++ q2.sh
@@ -8,4 +8,5 @@ fi
 while IFS='' read -r line || [[ -n "$line" ]]; do
     echo "Text read from file: $line"
     iptables -A INPUT -s $line -j DROP
+    echo "iptables -D INPUT -s $line -j DROP" | at now + $2 minutes
 done < "$1"
